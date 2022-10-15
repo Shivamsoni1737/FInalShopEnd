0a0336aa1c339f6344b309523d50d7fe5e3dd97b
Kartik Barmaiya <kartik.barmaiya28@gmail.com>
2022-10-15 20:18:08 +0530

    Store Details edit added

--- src/components/Dashboard/settings/storeprofile/Storeprofile.jsx
+++ src/components/Dashboard/settings/storeprofile/Storeprofile.jsx
@@ -12,7 +12,7 @@ import "react-date-picker/dist/DatePicker.css"
 const Storeprofile = ({store, setStore}) => {
 
   const[editStore, setEditStore] = useState(false)
-  const iter = [1,2,3,4,5];
+  const iter = [0,1,2,3,4];
 
   const submitHandler = () => {
     setEditStore(false);
@@ -37,26 +37,46 @@ const Storeprofile = ({store, setStore}) => {
           <p className='text-xs'>132 ratings </p>
         </div>
 
-        <form className='text-gray-700 text-sm md:text-base'>
+        <form className='text-gray-700 w-[100%] mt-4 text-sm md:text-base'>
+          <div className='flex my-3'>
+            <span className='font-semibold'>Website: </span>
+            <input onChange={(e)=>{setStore({...store, website: e.target.value})}} value={store.website} type="text" className='px-2 md:px-4 w-[8rem] md:w-[24rem] md:mx-2 border-b outline-none' />
+          </div>
+          <div className='flex my-3'>
+            <span className='font-semibold'>Description: </span>
+            <input onChange={(e)=>{setStore({...store, description: e.target.value})}} value={store.description} type="text" className='px-2 md:px-4 w-[8rem] md:w-[24rem] md:mx-2 border-b outline-none' />
+          </div>
           <div className='flex my-3'>
             <span className='font-semibold'>Email: </span>
-            <input onChange={(e)=>{setStore({...store, email: e.target.value})}} value={store.email} type="text" className='px-2 md:px-4 w-[8rem] md:w-full md:mx-2 border-b outline-none' />
-          </div>
-          <div className='flex my-3'>
-            <span className='font-semibold'>Contact: </span>
-            <input onChange={(e)=>{setStore({...store, contact: e.target.value})}} value={store.contact} type="number" className='px-2 md:px-4 w-[8rem] md:w-full md:mx-2 border-b outline-none' />
-          </div>
-          <div className='flex my-3'>
-            <span className='font-semibold'>DOB: </span>
             {/* <DatePicker
               selected={store.dob}
               onChange={date => setStore({...store, dob: date})}
             /> */}
-            <input onChange={(e)=>{setStore({...store, dob: e.target.value})}} value={store.dob} type="text" className='px-2 md:px-4 md:mx-2 w-[8rem] md:w-full border-b outline-none' />
+            <input onChange={(e)=>{setStore({...store, email: e.target.value})}} value={store.email} type="email" className='px-2 md:px-4 md:mx-2 w-[8rem] md:w-[24rem] border-b outline-none' />
+          </div>
+          <div className='flex my-3'>
+            <span className='font-semibold'>Contact: </span>
+            <input onChange={(e)=>{setStore({...store, contact: e.target.value})}} value={store.contact} type="text" className='px-2 md:px-4 md:mx-2 w-[8rem] md:w-[24rem] border-b outline-none' />
           </div>
           <div className='flex my-3'>
             <span className='font-semibold'>Address: </span>
-            <input onChange={(e)=>{setStore({...store, address: e.target.value})}} value={store.address} type="text" className='px-2 md:px-4 md:mx-2 w-[8rem] md:w-full border-b outline-none' />
+            <input onChange={(e)=>{setStore({...store, address: e.target.value})}} value={store.address} type="text" className='px-2 md:px-4 md:mx-2 w-[8rem] md:w-[24rem] border-b outline-none' />
+          </div>
+          <div className='flex my-3'>
+            <span className='font-semibold'>G-maps: </span>
+            <input onChange={(e)=>{setStore({...store, gmaps: e.target.value})}} value={store.gmaps} type="text" className='px-2 md:px-4 md:mx-2 w-[8rem] md:w-[24rem] border-b outline-none' />
+          </div>
+          <div className='flex my-3'>
+            <span className='font-semibold'>ZIP-Code: </span>
+            <input onChange={(e)=>{setStore({...store, zipcode: e.target.value})}} value={store.zipcode} type="text" className='px-2 md:px-4 md:mx-2 w-[8rem] md:w-[24rem] border-b outline-none' />
+          </div>
+          <div className='flex my-3'>
+            <span className='font-semibold'>Open on: </span>
+            <input onChange={(e)=>{setStore({...store, daysopened: e.target.value})}} value={store.daysopened} type="text" className='px-2 md:px-4 md:mx-2 w-[8rem] md:w-[24rem] border-b outline-none' />
+          </div>
+          <div className='flex my-3'>
+            <span className='font-semibold'>Timing: </span>
+            <input onChange={(e)=>{setStore({...store, timing: e.target.value})}} value={store.timing} type="text" className='px-2 md:px-4 md:mx-2 w-[8rem] md:w-[24rem] border-b outline-none' />
           </div>
           <button onClick={submitHandler} className="w-full flex justify-center items-center text-gray-600 hover:text-green-600 mt-8"><p className='mr-1'>Save Changes</p><TiTick className='text-lg text-green-600' /></button>
         </form>
@@ -78,8 +98,9 @@ const Storeprofile = ({store, setStore}) => {
           <p className='my-3 flex'><span className='font-semibold'>Email: </span><p className='px-4 mx-2 border-b border-white'>{store.email}</p> </p>
           <p className='my-3 flex'><span className='font-semibold'>Contact: </span><p className='px-4 mx-2 border-b border-white'>{store.contact}</p> </p>
           <p className='my-3 flex'><span className='font-semibold'>Address: </span><p className='px-4 mx-2 border-b border-white'>{store.address}</p> </p>
+          <p className='my-3 flex'><span className='font-semibold'>G-maps: </span><p className='px-4 mx-2 border-b border-white'>{store.gmaps}</p> </p>
           <p className='my-3 flex'><span className='font-semibold'>ZIP-code: </span><p className='px-4 mx-2 border-b border-white'>{store.zipcode}</p> </p>
-          <p className='my-3 flex'><span className='font-semibold'>Days Opened: </span><p className='px-4 mx-2 border-b border-white'>{store.daysopened}</p> </p>
+          <p className='my-3 flex'><span className='font-semibold'>Open on: </span><p className='px-4 mx-2 border-b border-white'>{store.daysopened}</p> </p>
           <p className='my-3 flex'><span className='font-semibold'>Timing: </span><p className='px-4 mx-2 border-b border-white'>{store.timing}</p> </p>
           <button onClick={()=> {setEditStore(true)}} className="w-full flex justify-center items-center text-gray-600 hover:text-blue-800 mt-8"><p className='mr-2'>Edit Details</p><AiOutlineEdit /></button>
         </div>
