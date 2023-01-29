75ec4f481fcba9a3f1a360bb30af0efecc529a30
Kartik Barmaiya <kartik.barmaiya28@gmail.com>
2023-01-29 23:23:20 +0530

    Shop form hover effects added

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -5,12 +5,12 @@ import {BsArrowUpRight, BsShopWindow} from "react-icons/bs"
 const Welcome = () => {
   return (
     <div className='min-h-screen flex items-center justify-center'>
-      <Link to="/" className='border flex flex-col items-center justify-center text-xl p-12 rounded-md'>
+      <Link to="/" className='border flex flex-col items-center justify-center hover:shadow-xl transition-all duration-700 hover:scale-110 hover:bg-gray-100 text-xl p-12 rounded-md'>
         <p className='mb-3'>Continue to Shopify</p>
         <BsArrowUpRight />
       </Link>
       <p className='mx-8'>OR</p>
-      <Link to="/list-shop" className='border flex flex-col items-center justify-center text-xl p-12 rounded-md '>
+      <Link to="/list-shop" className='border flex flex-col items-center justify-center hover:shadow-xl transition-all duration-700 hover:scale-110 hover:bg-gray-100 text-xl p-12 rounded-md '>
         <p className='mb-3'>List your shop/business</p>
         <BsShopWindow />
       </Link>
